37abc99264930f0f8f7e8be80a7213a7d563cdcc
Dayana Gil <dayanagil@Dayanas-MacBook-Pro.local>
2021-08-19 22:54:52 -0400

    added header component and links to home logo, shop and contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Header />
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route path='/shop' component={ ShopPage } />
